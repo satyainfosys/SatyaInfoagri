fe6d0cabb98e8725f85d368d985f4b50ecfc74ee
Shahnawaz Memon <shahnawaz.satyainfosys@gmail.com>
2023-05-03 17:36:31 +0530

    Integrated unit-list API - Implemented popup to add location details - Fixed some minor issues

--- src/components/Farmers/FarmersLandTable.js
+++ src/components/Farmers/FarmersLandTable.js
@@ -1,45 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Table, Form, Modal } from 'react-bootstrap';
+import { Button, Table, Form, Modal, Row } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { farmerLandDetailsAction } from 'actions';
 import { toast } from 'react-toastify';
+import axios from 'axios';
 
 export const FarmersLandTable = () => {
   const dispatch = useDispatch();
   const [formHasError, setFormError] = useState(false);
   const [rowData, setRowData] = useState([]);
+  const [locationRowData, setLocationRowData] = useState([{ id: 1, latitude: "", longitude: "" }]);
 
   const columnsArray = [
-    'Longitude',
-    'Latitude',
+    'Add',
     'Khasra No',
     'Land Mark',
     'Ownership',
     'Usage',
     'Org/Inorg',
     'Cultivated Land',
-    'Land Unit',
     'Active Status',
     'Action'
   ];
 
   const [modalShow, setModalShow] = useState(false);
   const [paramsData, setParamsData] = useState({});
+  const [showLocationModal, setShowLocationModal] = useState(false);
+  const [unitList, setUnitList] = useState([])
 
   const emptyRow = {
     id: rowData.length + 1,
     encryptedFarmerCode: localStorage.getItem("EncryptedFarmerCode") ? localStorage.getItem("EncryptedFarmerCode") : '',
     encryptedCompanyCode: localStorage.getItem("EncryptedCompanyCode") ? localStorage.getItem("EncryptedCompanyCode") : '',
-    encryptedClientCode: localStorage.getItem("EncryptedClientCode") ? localStorage.getItem("EncryptedClientCode") : '',
-    longitude: '',
-    latitude: '',
+    encryptedClientCode: localStorage.getItem("EncryptedClientCode") ? localStorage.getItem("EncryptedClientCode") : '',    
     khasraNo: '',
     landMark: '',
     ownerShip: '',
     usage: '',
     croppingType: '',
     landArea: '',
-    cultivatedLandUnit: '',
     activeStatus: '',
     addUser: localStorage.getItem("LoginUserName"),
     modifyUser: localStorage.getItem("LoginUserName")
@@ -52,6 +51,7 @@ export const FarmersLandTable = () => {
   const farmerError = farmerDetailsErrorReducer.farmerDetailsError;
 
   useEffect(() => {
+    getUnitList();
     setRowDataValue(farmerLandDetailsReducer, farmerLandDetailsData);
   }, [farmerLandDetailsData, farmerLandDetailsReducer]);
 
@@ -60,11 +60,12 @@ export const FarmersLandTable = () => {
   };
 
   const validateFarmerLandDetailsForm = () => {
+
     let isValid = true;
 
     if (farmerLandDetailsData && farmerLandDetailsData.length > 0) {
       farmerLandDetailsData.forEach((row, index) => {
-        if (!row.longitude || !row.latitude) {
+        if (!row.khasraNo || !row.ownerShip || !row.croppingType || !row.landArea) {
           isValid = false;
           setFormError(true);
         }
@@ -75,7 +76,8 @@ export const FarmersLandTable = () => {
   }
 
   const handleAddRow = () => {
-    if (validateFarmerLandDetailsForm()) {
+    let isValid = validateFarmerLandDetailsForm();
+    if (isValid) {
       farmerLandDetailsData.push(emptyRow);
       dispatch(farmerLandDetailsAction(farmerLandDetailsData))
     }
@@ -123,6 +125,31 @@ export const FarmersLandTable = () => {
     setModalShow(false);
   }
 
+  const localModalPreview = () => {
+    setShowLocationModal(true);
+  }
+
+  const addLocationRow = () => {
+    setLocationRowData([...locationRowData, { id: locationRowData.length + 1, latitude: "", longitude: "" }]);
+  };
+
+  const getUnitList = async () => {
+    let response = await axios.get(process.env.REACT_APP_API_URL + '/unit-list')
+    let unitListData = [];
+
+    if (response.data.status == 200) {
+      if (response.data && response.data.data.length > 0) {
+        response.data.data.forEach(units => {
+          unitListData.push({
+            key: units.unitName,
+            value: units.unitCode
+          })
+        })
+      }
+      setUnitList(unitListData);
+    }
+  }
+
   return (
     <>
       {modalShow && paramsData &&
@@ -146,174 +173,227 @@ export const FarmersLandTable = () => {
           </Modal.Footer>
         </Modal>
       }
-      <div style={{ display: 'flex', justifyContent: 'end' }}>
-        <Button
-          id="btnAddFarmersLandTable"
-          className="mb-2"
-          onClick={handleAddRow}
+      {showLocationModal &&
+        <Modal
+          show={showLocationModal}
+          onHide={() => setShowLocationModal(false)}
+          size="md"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+          backdrop="static"
         >
-          Add Land Details
-        </Button>
-      </div>
-
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">Location Details</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Table>
+                <thead>
+                  <tr>
+                    <th>Latitude</th>
+                    <th>Longitude</th>
+                    <th>
+                      <Button onClick={addLocationRow}>+</Button>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {locationRowData.map((row, index) => (
+                    <tr key={index}>
+                      <td key={index}>
+                        <Form.Control
+                          type="text"
+                          id="txtLatitude"
+                          name="latitude"
+                          // value={row.latitude}
+                          // onChange={(e) => handleFieldChange(e, index)}
+                          placeholder="Latitude"
+                          className="form-control"
+                        />
+                      </td>
+                      <td key={index}>
+                        <Form.Control
+                          type="text"
+                          id="txtLongitude"
+                          name="longitude"
+                          // value={row.longitude}
+                          // onChange={(e) => handleFieldChange(e, index)}
+                          placeholder="Longitude"
+                          className="form-control"
+                          required
+                        />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="success" onClick={() => setShowLocationModal(false)}>Cancel</Button>
+            <Button variant="danger">Save</Button>
+          </Modal.Footer>
+        </Modal>
+      }
+      <Row>
+        <div style={{ display: 'flex', justifyContent: 'left' }}>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Unit<span className="text-danger">*</span></Form.Label><br />
+            <Form.Select
+              type="text"
+              id="txtUnit"
+              className="form-control"
+            >
+              <option value=''>Select Unit</option>
+              {unitList.map((option, index) => (
+                <option key={index} value={option.value}>{option.key}</option>
+              ))}
+            </Form.Select>
+          </Form.Group>
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'end' }}>
+          <Button
+            id="btnAddFarmersLandTable"
+            className="mb-2"
+            onClick={handleAddRow}
+          >
+            Add Land Details
+          </Button>
+        </div>
+      </Row>
       <Form
         noValidate
         validated={formHasError}
         className="details-form"
         id="AddFarmersLiveStockTableDetailsForm"
       >
-        <Table striped responsive id="TableList" className="no-pb">
-          <thead>
-            <tr>
-              {columnsArray.map((column, index) => (
-                <th className="text-left" key={index}>
-                  {column}
-                </th>
-              ))}
-              <th />
-            </tr>
-          </thead>
-          <tbody id="tbody" className="details-form">
-            {rowData.map((farmerLandDetailsData, index) => (
+        {
+          farmerLandDetailsData && farmerLandDetailsData.length > 0 &&
+          <Table striped responsive id="TableList" className="no-pb">
+            <thead>
               <tr>
-                <td>
-                  <Form.Control
-                    type="text"
-                    id="textLongitude"
-                    name="longitude"
-                    value={farmerLandDetailsData.longitude}
-                    onChange={(e) => handleFieldChange(e, index)}
-                    placeholder="Longitude"
-                    className="form-control"
-                  />
-                </td>
-                <td>
-                  <Form.Control
-                    type="text"
-                    id="textLatitude"
-                    name="latitude"
-                    value={farmerLandDetailsData.latitude}
-                    onChange={(e) => handleFieldChange(e, index)}
-                    placeholder="Latitude"
-                    className="form-control"
-                  />
-                </td>
-                <td>
-                  <Form.Control
-                    type="text"
-                    id="txtKhasraNo"
-                    name="khasraNo"
-                    value={farmerLandDetailsData.khasraNo}
-                    onChange={(e) => handleFieldChange(e, index)}
-                    placeholder="Khasra No"
-                    className="form-control"
-                  />
-                </td>
-                <td>
-                  <Form.Control
-                    type="text"
-                    id="txtLandMark"
-                    name="landMark"
-                    value={farmerLandDetailsData.landMark}
-                    onChange={(e) => handleFieldChange(e, index)}
-                    placeholder="Land Mark"
-                    className="form-control"
-                  />
-                </td>
-
-                <td>
-                  <Form.Select
-                    type="text"
-                    id="txtOwnerShip"
-                    name="ownerShip"
-                    className="form-control"
-                    value={farmerLandDetailsData.ownerShip}
-                    onChange={(e) => handleFieldChange(e, index)}
-                  >
-                    <option value=''>Select</option>
-                    <option value='Owned'>Owned</option>
-                    <option value='Leased'>Leased</option>
-                  </Form.Select>
-                </td>
-
-                <td>
-                  <Form.Select
-                    type="text"
-                    id="txtUsage"
-                    name="usage"
-                    className="form-control"
-                    value={farmerLandDetailsData.usage}
-                    onChange={(e) => handleFieldChange(e, index)}
-                  >
-                    <option value=''>Select</option>
-                    <option value='Irrigated'>Irrigated</option>
-                    <option value='Unirrigated'>Unirrigated</option>
-                  </Form.Select>
-                </td>
-
-                <td>
-                  <Form.Select
-                    type="text"
-                    id="txtCroppingType"
-                    name="croppingType"
-                    value={farmerLandDetailsData.croppingType}
-                    onChange={(e) => handleFieldChange(e, index)}
-                    className="form-control"
-                  >
-                    <option value=''>Select</option>
-                    <option value="Organic">Organic</option>
-                    <option value="Inorganic">Inorganic</option>
-                  </Form.Select>
-                </td>
-
-                <td>
-                  <Form.Control
-                    type="num"
-                    id="txtLandArea"
-                    name="landArea"
-                    value={farmerLandDetailsData.landArea}
-                    onChange={(e) => handleFieldChange(e, index)}
-                    placeholder="Cultivated Land"
-                  />
-                </td>
-                <td>
-                  <Form.Select
-                    type="text"
-                    id="txtCultivatedLandUnit"
-                    name="cultivatedLandUnit"
-                    value={farmerLandDetailsData.cultivatedLandUnit}
-                    onChange={(e) => handleFieldChange(e, index)}
-                    className="form-control"
-                  >
-                    <option value=''>Select</option>
-                    <option value="Hectare">Hectare</option>
-                    <option value="Bhiga">Bhiga</option>
-                    <option value="Kila">Kila</option>
-                  </Form.Select>
-                </td>
-
-                <td>
-                  <Form.Select
-                    id="txtStatus"
-                    name="activeStatus"
-                    className="form-control"
-                    value={farmerLandDetailsData.activeStatus}
-                    onChange={(e) => handleFieldChange(e, index)}
-                  >
-                    <option value="Active">Active</option>
-                    <option value="Suspended">Suspended</option>
-                  </Form.Select>
-                </td>
-
-                <td>
-                  <i className="fa fa-trash" onClick={() => { ModalPreview(farmerLandDetailsData.encryptedFarmerLandCode) }} />
-                </td>
+                {columnsArray.map((column, index) => (
+                  <th className="text-left" key={index}>
+                    {column}
+                  </th>
+                ))}
+                <th />
               </tr>
-            ))
-            }
+            </thead>
+            <tbody id="tbody" className="details-form">
+              {rowData.map((farmerLandDetailsData, index) => (
+                <tr key={index}>
+                  <td key={index}>
+                    <Button style={{ fontSize: '10px' }}
+                      onClick={() => { localModalPreview() }} >
+                      Add Location
+                    </Button>
+                  </td>
+                  <td key={index}>
+                    <Form.Control
+                      type="text"
+                      id="txtKhasraNo"
+                      name="khasraNo"
+                      value={farmerLandDetailsData.khasraNo}
+                      onChange={(e) => handleFieldChange(e, index)}
+                      placeholder="Khasra No"
+                      className="form-control"
+                      required
+                    />
+                  </td>
+                  <td key={index}>
+                    <Form.Control
+                      type="text"
+                      id="txtLandMark"
+                      name="landMark"
+                      value={farmerLandDetailsData.landMark}
+                      onChange={(e) => handleFieldChange(e, index)}
+                      placeholder="Land Mark"
+                      className="form-control"
+                    />
+                  </td>
 
-          </tbody>
-        </Table>
+                  <td key={index}>
+                    <Form.Select
+                      type="text"
+                      id="txtOwnerShip"
+                      name="ownerShip"
+                      className="form-control"
+                      value={farmerLandDetailsData.ownerShip}
+                      onChange={(e) => handleFieldChange(e, index)}
+                      required
+                    >
+                      <option value=''>Select</option>
+                      <option value='Owned'>Owned</option>
+                      <option value='Leased'>Leased</option>
+                    </Form.Select>
+                  </td>
+
+                  <td key={index}>
+                    <Form.Select
+                      type="text"
+                      id="txtUsage"
+                      name="usage"
+                      className="form-control"
+                      value={farmerLandDetailsData.usage}
+                      onChange={(e) => handleFieldChange(e, index)}
+                    >
+                      <option value=''>Select</option>
+                      <option value='Irrigated'>Irrigated</option>
+                      <option value='Unirrigated'>Unirrigated</option>
+                    </Form.Select>
+                  </td>
+
+                  <td key={index}>
+                    <Form.Select
+                      type="text"
+                      id="txtCroppingType"
+                      name="croppingType"
+                      value={farmerLandDetailsData.croppingType}
+                      onChange={(e) => handleFieldChange(e, index)}
+                      className="form-control"
+                      required
+                    >
+                      <option value=''>Select</option>
+                      <option value="Organic">Organic</option>
+                      <option value="Inorganic">Inorganic</option>
+                    </Form.Select>
+                  </td>
+
+                  <td key={index}>
+                    <Form.Control
+                      type="num"
+                      id="txtLandArea"
+                      name="landArea"
+                      value={farmerLandDetailsData.landArea}
+                      onChange={(e) => handleFieldChange(e, index)}
+                      placeholder="Cultivated Land"
+                      required
+                    />
+                  </td>
+                  <td key={index}>
+                    <Form.Select
+                      id="txtStatus"
+                      name="activeStatus"
+                      className="form-control"
+                      value={farmerLandDetailsData.activeStatus}
+                      onChange={(e) => handleFieldChange(e, index)}
+                    >
+                      <option value="Active">Active</option>
+                      <option value="Suspended">Suspended</option>
+                    </Form.Select>
+                  </td>
+
+                  <td>
+                    <i className="fa fa-trash" onClick={() => { ModalPreview(farmerLandDetailsData.encryptedFarmerLandCode) }} />
+                  </td>
+                </tr>
+              ))
+              }
+
+            </tbody>
+          </Table>
+        }
       </Form>
     </>
   );
