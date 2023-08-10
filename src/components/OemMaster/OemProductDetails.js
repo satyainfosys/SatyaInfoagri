a8489792fc74e429b313daee030b4baf23c47f99
Shahnawaz Memon <shahnawaz.satyainfosys@gmail.com>
2023-08-10 13:27:54 +0530

    Implemented design for popup on Add Info button click - Added design for OEM product details

--- src/components/OemMaster/OemProductDetails.js
+++ src/components/OemMaster/OemProductDetails.js
@@ -1,7 +1,7 @@
 import { oemProductDetailsAction } from 'actions';
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Button, Table, Form, Modal, Card } from 'react-bootstrap';
+import { Button, Table, Form, Modal, Card, Row, Col } from 'react-bootstrap';
 import EnlargableTextbox from 'components/common/EnlargableTextbox';
 import FalconCardHeader from 'components/common/FalconCardHeader';
 import { toast } from 'react-toastify';
@@ -12,6 +12,7 @@ const OemProductDetails = () => {
 
     const dispatch = useDispatch();
     const [rowData, setRowData] = useState([]);
+    const [oemModal, setOemModal] = useState(false);
 
     const emptyRow = {
         id: rowData.length + 1,
@@ -46,16 +47,205 @@ const OemProductDetails = () => {
         'Add Info'
     ];
 
+    useEffect(() => {
+        if (oemProductDetailsReducer.oemProductDetails.length > 0) {
+            setRowData(oemProductData);
+        }
+    }, [oemProductData, oemProductDetailsReducer])
+
     const handleAddRow = async () => {
         oemProductData.unshift(emptyRow);
         dispatch(oemProductDetailsAction(oemProductData));
     }
 
+    const oemDetailModalPreview = () => {
+        setOemModal(true);
+    }
+
     return (
         <>
+            {
+                oemModal &&
+                <Modal
+                    show={oemModal}
+                    onHide={() => setOemModal(false)}
+                    size="md"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered
+                    backdrop="static"
+                >
+                    <Modal.Header closeButton>
+                        <Modal.Title id="contained-modal-title-vcenter">OEM - Amazon</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className="max-five-rows">
+                        <Form
+                            noValidate
+                            // validated={modalError}
+                            className="details-form"
+                        >
+                            <Row>
+                                <div className="mb-2 d-flex justify-content-center align-items-center">Item - Item Name</div>
+                                <hr></hr>
+                                <Col className="me-3 ms-3 mb-3 mt-2" md="11">
+                                    <Form.Group as={Row} className="mb-1" controlId="formPlaintextPassword">
+                                        <Form.Label column sm="3">
+                                            Qty
+                                        </Form.Label>
+                                        <Col sm="4">
+                                            <Form.Control id="txtQty" name="quantity" placeholder="Quantity" />
+                                        </Col>
+                                        <Col sm="4">
+                                            <Form.Select
+                                                type="text"
+                                                id="txtUnit"
+                                                name="unit"
+                                                className="form-control"
+                                            >
+                                                <option value=''>Select</option>
+                                            </Form.Select>
+                                        </Col>
+                                    </Form.Group>
+
+                                    <Form.Group as={Row} className="mb-1" controlId="formPlaintextPassword">
+                                        <Form.Label column sm="3">
+                                            Maturity Days
+                                        </Form.Label>
+                                        <Col sm="4">
+                                            <Form.Control id="txtNoOfDays" name="noOfDays" placeholder="Maturity Days" />
+                                        </Col>
+                                        <Col sm="4">
+                                            <Form.Select
+                                                type="text"
+                                                id="txtUnitDays"
+                                                name="unitDays"
+                                                className="form-control"
+                                            >
+                                                <option value=''>Select</option>
+                                            </Form.Select>
+                                        </Col>
+                                    </Form.Group>
+
+                                    <Form.Group as={Row} className="mb-1" controlId="formPlaintextPassword">
+                                        <Form.Label column sm="3">
+                                            Yield Land
+                                        </Form.Label>
+                                        <Col sm="4">
+                                            <Form.Control id="txtYieldLand" name="yieldLand" placeholder="Yield Land" />
+                                        </Col>
+                                        <Col sm="4">
+                                            <Form.Select
+                                                type="text"
+                                                id="txtLandUnit"
+                                                name="landUnits"
+                                                className="form-control"
+                                            >
+                                                <option value=''>Select</option>
+                                            </Form.Select>
+                                        </Col>
+                                    </Form.Group>
+
+                                    <Form.Group as={Row} className="mb-1" controlId="formPlaintextPassword">
+                                        <Form.Label column sm="3">
+                                            Yield Output
+                                        </Form.Label>
+                                        <Col sm="4">
+                                            <Form.Control id="txtYieldOutput" name="yieldOutput" placeholder="Yield Output" />
+                                        </Col>
+                                        <Col sm="4">
+                                            <Form.Select
+                                                type="text"
+                                                id="txtYieldOutputUnits"
+                                                name="yieldOutputUnits"
+                                                className="form-control"
+                                            >
+                                                <option value=''>Select</option>
+                                            </Form.Select>
+                                        </Col>
+                                    </Form.Group>
+                                </Col>
+
+                                <Table
+                                    style={{ paddingLeft: 0 }}
+                                    striped bordered responsive className="text-nowrap tab-page-table">
+                                    <thead className='custom-bg-200'>
+                                        <tr>
+                                            <th>Soil Ingredients</th>
+                                            <th>From</th>
+                                            <th>To</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                N
+                                            </td>
+                                            <td>
+                                                <Form.Control placeholder="N From" />
+                                            </td>
+                                            <td>
+                                                <Form.Control placeholder="N To" />
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                P
+                                            </td>
+                                            <td>
+                                                <Form.Control placeholder="P From" />
+                                            </td>
+                                            <td>
+                                                <Form.Control placeholder="P To" />
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                K
+                                            </td>
+                                            <td>
+                                                <Form.Control placeholder="K From" />
+                                            </td>
+                                            <td>
+                                                <Form.Control placeholder="K To" />
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                PH
+                                            </td>
+                                            <td>
+                                                <Form.Control placeholder="PH From" />
+                                            </td>
+                                            <td>
+                                                <Form.Control placeholder="PH To" />
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                Temp
+                                            </td>
+                                            <td>
+                                                <Form.Control placeholder="Temp From" />
+                                            </td>
+                                            <td>
+                                                <Form.Control placeholder="Temp To" />
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </Table>
+
+                            </Row>
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="success" >Save</Button>
+                        <Button variant="danger" onClick={() => { setOemModal(false) }} >Cancel</Button>
+                    </Modal.Footer>
+                </Modal >
+            }
+
             <Card className="h-100 mb-2">
                 <FalconCardHeader
-                    title="Family Details"
+                    title="OEM Product Details"
                     titleTag="h6"
                     className="py-2"
                     light
@@ -225,7 +415,8 @@ const OemProductDetails = () => {
                                             </td>
 
                                             <td>
-                                                <FontAwesomeIcon icon={'plus'} className="fa-2x" />
+                                                <FontAwesomeIcon icon={'plus'} className="fa-2x"
+                                                    onClick={oemDetailModalPreview} />
                                             </td>
                                         </tr>
                                     ))}
