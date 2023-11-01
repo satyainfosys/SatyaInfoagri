8941a0853baacf25695144ace5ee467840c1f8e7
Shahnawaz Memon <shahnawaz.satyainfosys@gmail.com>
2023-11-01 18:44:12 +0530

    Integrated get-material-receipt-header-list API in Crop Purchase List tab

--- src/components/CropPurchase/CropPurchase.js
+++ src/components/CropPurchase/CropPurchase.js
@@ -12,7 +12,10 @@ const listColumnArray = [
     { accessor: 'materialReceiptId', Header: 'Material Receipt No' },
     { accessor: 'vendorName', Header: 'Vendor Name' },
     { accessor: 'materialReceiptDate', Header: 'Delivery Date' },
-    { accessor: 'personName', Header: 'Person Name' }
+    { accessor: 'personName', Header: 'Person Name' },
+    { accessor: 'farmerName', Header: 'Farmer Name' },
+    { accessor: 'farmerFatherName', Header: 'Farmer Father Name' },
+    { accessor: 'farmerPhoneNumber', Header: 'Farmer Phone Name' },
 ]
 
 const CropPurchase = () => {
@@ -22,6 +25,7 @@ const CropPurchase = () => {
     const [perPage, setPerPage] = useState(15);
     const [activeTabName, setActiveTabName] = useState();
     const [companyList, setCompanyList] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         $('[data-rr-ui-event-key*="Add Crop Purchase"]').attr('disabled', true);
@@ -51,7 +55,7 @@ const CropPurchase = () => {
             }
             setCompanyList(companyData)
             if (companyResponse.data.data.length == 1) {
-                // fetchMaterialReceiptHeaderList(1, perPage, companyResponse.data.data[0].encryptedCompanyCode);
+                fetchMaterialReceiptHeaderList(1, perPage, companyResponse.data.data[0].encryptedCompanyCode);
                 // getVendorMasterList(companyResponse.data.data[0].encryptedCompanyCode)
                 localStorage.setItem("CompanyName", companyResponse.data.data[0].companyName)
                 localStorage.setItem("EncryptedCompanyCode", companyResponse.data.data[0].encryptedCompanyCode);
@@ -66,8 +70,32 @@ const CropPurchase = () => {
         const selectedOption = e.target.options[e.target.selectedIndex];
         const selectedKey = selectedOption.dataset.key || selectedOption.label;
         localStorage.setItem("CompanyName", selectedKey)
-        // fetchMaterialReceiptHeaderList(1, perPage, e.target.value);
+        fetchMaterialReceiptHeaderList(1, perPage, e.target.value);
         // getVendorMasterList(e.target.value);
+    }
+
+    const fetchMaterialReceiptHeaderList = async (page, size = perPage, encryptedCompanyCode) => {
+        let token = localStorage.getItem('Token');
+
+        const listFilter = {
+            pageNumber: page,
+            pageSize: size,
+            EncryptedCompanyCode: encryptedCompanyCode,
+            IsCropPurchase: true
+        }
+
+        setIsLoading(true);
+        let response = await axios.post(process.env.REACT_APP_API_URL + '/get-material-receipt-header-list', listFilter, {
+            headers: { Authorization: `Bearer ${JSON.parse(token).value}` }
+        })
+
+        if (response.data.status == 200) {
+            setIsLoading(false);
+            setListData(response.data.data);
+        } else {
+            setIsLoading(false);
+            setListData([])
+        }
     }
 
     $('[data-rr-ui-event-key*="Crop Purchase List"]').off('click').on('click', function () {
